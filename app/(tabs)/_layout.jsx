3366a5c7793ba66ed6f1611f27cc8e4c1c219eaa
Nishant Rathore <rnishant428@gmail.com>
2025-06-20 20:14:43 +0530

    first commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,40 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Colors } from "../../assets/Colors";
+
+const TabLayout = () => {
+    return (
+        <Tabs
+            screenOptions={() => ({
+                headerShown: false,
+                tabBarActiveTintColor: Colors.PRIMARY,
+                tabBarInactiveTintColor: Colors.dark.text,
+                tabBarStyle: {
+                    backgroundColor: Colors.SECONDARY,
+                    height: 80,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: 'bold',
+                }
+            })}>
+            <Tabs.Screen name="home" options={{
+                title: "Home", tabBarIcon: ({ color }) => (
+                    <Ionicons name="home" size={24} color={color} />
+                )
+            }} />
+            <Tabs.Screen name="history" options={{
+                title: "History", tabBarIcon: ({ color }) => (
+                    <Ionicons name="time" size={24} color={color} />
+                )
+            }} />
+            <Tabs.Screen name="profile" options={{
+                title: "Profile", tabBarIcon: ({ color }) => (
+                    <Ionicons name="person-sharp" size={24} color={color} />
+                )
+            }} />
+        </Tabs >
+    )
+}
+
+export default TabLayout
---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity onPress={()=>router.push('/home')}>
+        <Text>Navigate</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
